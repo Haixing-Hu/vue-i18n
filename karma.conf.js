15674b40e30f9e7066e6ea419a3437fb73ccfeb0
Haixing Hu <starfish.hu@gmail.com>
2015-10-12 23:19:23 +0800

    ignore the code coverages of scripts in the lib directory

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function (config) {
       module: {
         postLoaders: [{
           test: /\.js$/,
-          exclude: /test|node_modules/,
+          exclude: /test|node_modules|lib/,
           loader: 'istanbul-instrumenter'
         }]
       }
